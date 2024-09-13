aacfc2fc0611ae7a936d096f1479fa7c2dc12834
hiki7 <rsaliyev03@gmail.com>
2024-09-14 01:32:08 +0500

    add: dockerfile and docker-compose

--- src/config/db_connect.py
+++ src/config/db_connect.py
@@ -4,7 +4,7 @@ from sqlmodel import create_engine
 def get_url():
     user = 'yadmin'
     password = 'qwerty123'
-    server = 'localhost'
+    server = 'db'
     port = '5432'
     db = 'saniraq-kz'
     return f"postgresql+psycopg://{user}:{password}@{server}:{port}/{db}"
